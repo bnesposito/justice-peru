c087297a112eacc4490c418a96dfd3e43f20ed9d
bnesposito <bnesposito2@gmail.com>
2018-01-11 18:04:34 -0500

    Clean data. Implement LDA.

--- code/main.py
+++ code/main.py
@@ -1,30 +1,50 @@
 import numpy as np
-import os
+import pickle
 import time
+import logging
+import pprint
 
 import work_data
 import config
 
 
 def main():
-    logger = config.config_logger(__name__, 30)
+    logger = config.config_logger(__name__, 10)
     t0 = time.time()
 
     pdf_path = './data/pdf/'
     txt_path = './data/txt/'
+    pkl_path = './data/pickle/'
+
+    convert_files = False
 
     logger.info('Beginning execution')
-    logger.info('Stage 0: .pdf to .txt')
-    logger.info('Pdf path: {0}'.format(pdf_path))
-    pdf_list = os.listdir(pdf_path)
+    if convert_files:
+        logger.info('Coonverting files: {0}'.format(convert_files))
+        logging.getLogger().setLevel(30)
+        all_docs = work_data.convert_pdf_to_txt(pdf_path, txt_path)
+        logging.getLogger().setLevel(10)
+        testimonies = open(pkl_path + 'testimonies.pkl', 'wb')
+        pickle.dump(all_docs, testimonies)
+        testimonies.close()
+    else:
+        testimonies = open(pkl_path + 'testimonies.pkl', 'rb')
+        all_docs = pickle.load(testimonies)
+        testimonies.close()
 
-    for index, pdf_name in enumerate(pdf_list):
-        data = work_data.pdfparser(pdf_path + pdf_name)
-        temp_name = os.path.splitext(pdf_name)[0]
-        temp = open(txt_path + '{0}.txt'.format(temp_name), 'w')
-        temp.write(data)
-        temp.close()
-        print('{0}. {1} - len: {2}\n{3}'.format(index, temp_name, len(data), data[:100]))
+    logger.info('Number of testimonies: {0}'.format(len(all_docs)))
+    # TODO filter data before cleaning (first ~5 lines are pointless)
+    clean_docs = [work_data.clean_data(doc).split() for doc in all_docs]
+    print(clean_docs[0])
+
+    mat_docs, dictionary = work_data.list_to_matrix(clean_docs)
+    print(mat_docs[0])
+    pprint.pprint(dictionary.token2id)
+    hi
+
+    lda_model = work_data.lda_model(dictionary, mat_docs)
+    print(lda_model)
+    pprint.pprint(lda_model.print_topics(num_topics=3, num_words=10))
 
     config.time_taken_display(t0)
 
--- code/work_data.py
+++ code/work_data.py
@@ -2,7 +2,24 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
 from pdfminer.layout import LAParams
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from gensim.models.ldamodel import LdaModel
+from nltk.stem import SnowballStemmer
+from six import iteritems
+from pattern.es import parsetree
+import gensim
+import string
 import io
+import os
+import pprint
+
+import config
+
+#import nltk
+#nltk.download('wordnet')
+
+logger = config.config_logger(__name__, 10)
 
 
 def pdfparser(data):
@@ -22,3 +39,50 @@ def pdfparser(data):
         data = retstr.getvalue()
 
     return data
+
+
+def convert_pdf_to_txt(pdf_path, txt_path):
+    logger.info('Stage 0: .pdf to .txt')
+    logger.info('Pdf path: {0}'.format(pdf_path))
+    pdf_list = os.listdir(pdf_path)
+    all_txt = []
+
+    for index, pdf_name in enumerate(pdf_list):
+        data = pdfparser(pdf_path + pdf_name)
+        all_txt.append(data)
+        temp_name = os.path.splitext(pdf_name)[0]
+        temp = open(txt_path + '{0}.txt'.format(temp_name), 'w')
+        temp.write(data)
+        temp.close()
+        print('{0}. {1} - len: {2}\n{3}'.format(index, temp_name, len(data), data[:100]))
+    return all_txt
+
+
+def clean_data(doc):
+    stop = set(stopwords.words('spanish'))
+    exclude = set(string.punctuation)
+    exclude.update(['«', '¿', '»', '¡', '…', '“', '—', '_', '‘', '–', '’'])
+    #lemma = WordNetLemmatizer()
+    #stemmer = SnowballStemmer('spanish')
+    parsetree("buena", lemmata=True)
+    stop_free = ' '.join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = ' '.join(parsetree(word, lemmata=True) for word in punc_free.split())
+    return normalized
+
+
+def list_to_matrix(doc):
+    dictionary = gensim.corpora.Dictionary(doc)
+    once_ids = [tokenid for tokenid, docfreq in iteritems(dictionary.dfs) if docfreq == 1]
+    dictionary.filter_tokens(once_ids)
+    dictionary.compactify()
+
+    logger.info('Unique words: {0}'.format(len(dictionary.keys())))
+    doc_term_matrix = [dictionary.doc2bow(my_doc) for my_doc in doc]
+    return doc_term_matrix, dictionary
+
+
+def lda_model(dictionary, matrix):
+    ldamodel = LdaModel(matrix, num_topics=3, id2word=dictionary, passes=50)
+    return ldamodel
+
